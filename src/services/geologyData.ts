bef757f2d5870f274def29cb678e294ad9155693
LYVON <lyvor.saas@gmail.com>
2026-06-29 00:47:38 -0300

    Add geology data service functions

---
+++ src/services/geologyData.ts
@@ -0,0 +1,6 @@
+import content from '../data/geology-content.json'
+
+export const getEras = () => content.eras
+export const getKnowledge = () => content.knowledge
+export const getMetrics = () => content.metrics
+export const getLayers = () => content.layers
